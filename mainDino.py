2c38fe774a3578e6a0b1f0fe2118cfe810bb97e4
yarikgg <97395432+yarikgg@users.noreply.github.com>
2022-01-23 16:56:35 +0300

    Update mainDino.py

--- mainDino.py
+++ mainDino.py
@@ -10,15 +10,11 @@ pygame.display.set_caption('chrome://dino')
 # игровые обьекты
 dino = pygame.image.load('dino.png')
 road = pygame.image.load('road.png')
-cactus = pygame.image.load('big_cactus.png')
-big_cactus = pygame.image.load('big_cactus.png')
 anim = pygame.image.load('anim1.png')
 anim2 = pygame.image.load('anim2.png')
 dead = pygame.image.load("smert'.png")
 # скорость движения кактусов
 speed = 1
-#переменная для большого кактуса
-bigc = 800
 
 hi = 60
 rx = 0
@@ -33,8 +29,7 @@ def graviti():
         if y >= 420:
             y = 420
 
-i = 0 #активация большого кактуса
-bigger_i = 0 # активация 3 кактусов
+
 
 cx = 800
 
@@ -76,10 +71,6 @@ while start:
         pygame.display.update()
         time.sleep(3)
         exit()
-    if bigc <= 0:
-        bigc = random.randint(800, 1090)
-        hi += 10
-        i = 1
     rx -= speed
     if rx <= -400:
         rx = 0
@@ -98,7 +89,6 @@ while start:
 
     display.blit(road,(rx, 450))
     display.blit(cactus,(cx, 420))
-    display.blit(big_cactus,(bigc, 420))
     pygame.display.update()
 
     pygame.time.delay(5)
